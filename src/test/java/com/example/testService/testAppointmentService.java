41f92fc200576a9d23337111046260e30643fc77
Md. Amanullah Islam <amanullah435islam@gmail.com>
2026-08-15 13:05:09 +0600

    update ExtendWith

--- src/test/java/com/example/testService/testAppointmentService.java
+++ src/test/java/com/example/testService/testAppointmentService.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -16,7 +16,8 @@ import static org.mockito.Mockito.when;
 
 
 
-@ExtendWith(SpringExtension.class)
+//@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 public class testAppointmentService {
 
     @Mock
